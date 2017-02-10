2b8cd79993d4554c6ddf2273a2251f9a2f752254
dwnldbot <dwnldbot@openmailbox.org>
2017-02-10 09:50:35 +0100

    More Changes

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 pacman -Syu
 
 # Install programs
-pacman -S vim mutt git firefox xorg-server xorg-server-utils xorg-xinit arc-gtk-theme termite feh sudo htop
+pacman -S vim mutt git firefox xorg-server xorg-server-utils xorg-xinit arc-gtk-theme termite feh sudo htop alsa-utils mesa cmus polkit apvlv zip unzip
 
 # Vim
 bash ./vim.sh
@@ -15,14 +15,15 @@ cp Xresources .Xresources
 
 # pacaur
 bash ./pacaur.sh
-pacaur -S i3-gaps i3blocks
+pacaur -S i3-gaps i3blocks light
 pacman -S i3lock
 
 # i3
 mkdir -p ~/.config/i3
 cp -r ./i3/* ~/.config/i3
+#TODO COPY i3blocklets
 #TODO fontawesome
-cp /etc/X11/xinit # TODO PATH
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo "exec i3" >> ~/.xinitrc
 
 # Termite
@@ -35,8 +36,15 @@ cp -r ./gtk-3.0/* ~/.config/gtk-3.0
 
 # firmware
 pacaur -S broadcom-wl-dkms aic94xx-firmware
-
 mkinitcpio -p linux
 
 
+useradd -m -g users -G wheel,games,power,optical,storage,scanner,lp,audio,video -s /bin/bash b0b
+passwd b0b
+
+# TODO SUDO
+
+#alsa
+echo -e "pcm.!default {\n\ttype hw\n\tcard Generic\n}\nctl.!default {\n\ttype hw\n\tcard Generic\n}" > /etc/asound.conf
+echo -e "defaults.pcm.card 1\ndefaults.ctl.card 1" > ~/.asoundrc
 
